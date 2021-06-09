a7d9cb6b8cd20ab5b7feb88dcaaf08d82e2d8949
James Arthur <jamesarthur7@icloud.com>
2021-06-09 18:52:57 +0100

    meeting scribblings

--- src/matrixRoute.lean
+++ src/matrixRoute.lean
@@ -6,8 +6,10 @@ open_locale matrix
 variables {m n o p R : Type*} [fintype m] [fintype n] [fintype o] [fintype p]
 
 /-- A matrix with added rows is still a matrix over the same type.  -/
-def matrix.reindex_or_zero [has_zero R] (A : matrix m n R) (fm : m ↪ o) (fn : n ↪ p) : matrix o p R := sorry
+def matrix.reindex_or_zero [has_zero R] (A : matrix m n R) (fm : m ↪ o) (fn : n ↪ p) 
+  : matrix o p R := sorry
 
+-- call this something different
 /-- A smith normal form is a diagonal that doesn't have to be square and can have diagonal zeroes -/
 def smith_normal [has_zero R] {r : ℕ} (a : fin r → R)
   (fm : fin r ↪ m) (fn : fin r ↪ n) : matrix m n R :=
@@ -15,7 +17,7 @@ def smith_normal [has_zero R] {r : ℕ} (a : fin r → R)
 
 variables [integral_domain R] [is_principal_ideal_ring R]
 
-/-- Every matrix over a pid has a smith normal form -/
+/-- Every matrix over a pid has a smith normal form. Lemma 3.8 -/
 lemma pid_matrix_has_smith [decidable_eq n] [decidable_eq m] (A : matrix m n R) : 
   ∃ (S : units (matrix m m R)) (T : units (matrix n n R))
     {r : ℕ} (a : fin r → R) (ha : sorry) -- the dvd condition
@@ -26,4 +28,10 @@ begin
   
   sorry
 end
+
+variables (r : ℕ) (i j : fin r) (a : fin r → R)
+
+#exit
+
+#check i ≤ j → (a i | a j) 
 
