9fc11dc34702f3132192906bef1c2ddd0922ee8a
James Arthur <jamesarthur7@icloud.com>
2021-06-09 19:24:49 +0100

    n is now the dim R

--- src/torsionRoute.lean
+++ src/torsionRoute.lean
@@ -2,7 +2,9 @@ import linear_algebra.free_module linear_algebra.std_basis data.real.basic
 
 -- we need that a free finitely generated module is isomorphic to ℝⁿ.
 
-variables {R M : Type*} [integral_domain R] [is_principal_ideal_ring R] [add_comm_group M] [module R M]
+variables {R M : Type*} [integral_domain R] [is_principal_ideal_ring R] 
+   [add_comm_group M] [field R] -- I added field here, it *may* break things
+   [module R M]
 
 open module
 
@@ -17,7 +19,7 @@ variables [module.finite R M] [no_zero_smul_divisors R M]
    'vector space'.
 -/
 
-variables {ι : Type*} (v : ι → R)
+variables {ι : Type*} [fintype ι] (v : ι → R)
 /- I need to create an orderng, this is done by inputting an element of type ι and then 
    getting out the element of type R (our 'vector space')
 -/
@@ -29,13 +31,20 @@ variables (b : basis ι R M) (i : ι)
 #check b
 #check M ≃ₗ[R] (ι →₀ R)
 
-open finite_dimensional
+open module
+
+#check finite_dimensional.finrank R M
+
+-- I'm missing something along the lines of, if a module is finitely generated
+-- and free, then it has a finite basis
+
+-- The way to create a free module is to just define a basis over the module
 
 /- Now it's time to state that M is isomorphic to Rⁿ -/
-noncomputable def M_iso_Rn (b : basis ι R M) (i : ι) (n : finrank R M) -- n must be the size of b
+noncomputable def M_iso_Rn (b : basis ι R M) (i : ι) (hn : n = finite_dimensional.finrank R M) -- n must be the size of b
   : M ≃ₗ[R] (fin n → R) := { 
-  to_fun := _,
-  inv_fun := λ x, b i, -- use the basis to prove tha
+  to_fun := _, 
+  inv_fun := λ x, b i, -- use the basis to prove this
   map_smul' := _,
   map_add' := _,
   left_inv := _,
